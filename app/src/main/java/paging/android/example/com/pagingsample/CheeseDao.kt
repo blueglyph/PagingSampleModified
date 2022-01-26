48173fe3eb8e37f057275e2054686c1f64121ac4
blueglyph <blueglyph@github.com>
2022-01-26 11:37:34 +0100

    add complicated logic for load() to mimic Room-generated PagingSource behaviour
    => seems to work
    
    placeholders must be enabled, or it won't work correctly.

--- app/src/main/java/paging/android/example/com/pagingsample/CheeseDao.kt
+++ app/src/main/java/paging/android/example/com/pagingsample/CheeseDao.kt
@@ -66,6 +66,9 @@ class CheeseDaoLocal {
 
     private class CheeseDataSource(val dao: CheeseDaoLocal, val pageSize: Int): PagingSource<Int, Cheese>() {
         fun max(a: Int, b: Int): Int = if (a > b) a else b
+        fun min(a: Int, b: Int): Int = if (a < b) a else b
+        var count = dao.count()
+        val UNDEF = -2147483648
 
         override fun getRefreshKey(state: PagingState<Int, Cheese>): Int? {
             val key = state.anchorPosition
@@ -74,14 +77,52 @@ class CheeseDaoLocal {
         }
 
         override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Cheese> {
-            val pageNumber = params.key ?: 0
-            val count = dao.count()
-            val data = dao.allCheesesOrdName().drop(pageNumber).take(pageSize)
-            return LoadResult.Page(
-                data = data,
-                prevKey = if (pageNumber > 0) max(0, pageNumber - pageSize) else null,
-                nextKey = if (pageNumber + pageSize < count) pageNumber + pageSize else null
+            data class Args(
+                var start: Int = 0, var size: Int = 0, var prevKey: Int? = null, var nextKey: Int? = null,
+                var itemsBefore: Int = UNDEF, var itemsAfter: Int = UNDEF
             )
+            val pos = params.key ?: 0
+            val args = Args()
+            when (params) {
+                is LoadParams.Append -> {
+                    args.start = pos
+                    args.prevKey = params.key
+                    //args.nextKey = if (args.start < count) min(args.start + params.loadSize, count) else null
+                    args.nextKey = if (args.start + params.loadSize < count) args.start + params.loadSize else null
+                }
+                is LoadParams.Prepend -> {
+                    args.start = max(pos - pageSize, 0)
+                    args.prevKey = if (args.start > 0) args.start else null
+                    args.nextKey = params.key
+                }
+                is LoadParams.Refresh -> {
+                    args.start = max((pos - params.loadSize/2)/pageSize*pageSize, 0)
+                    args.prevKey = if (args.start > 0) args.start else null
+                    args.nextKey = if (args.start + params.loadSize < count) min(args.start + params.loadSize, count - 1) else null
+                }
+            }
+            args.size = min(params.loadSize, count - args.start)
+            if (params is LoadParams.Refresh) {
+                args.itemsBefore = args.start
+                args.itemsAfter = count - args.size - args.start
+            }
+            val source = dao.allCheesesOrdName()
+            val data = source.drop(args.start).take(args.size)
+            if (params.key == null && data.count() == 0) {
+                Log.d("CHEESE_SRC", "load -> Error (no data) source: ${source.count()} item(s)")
+                return LoadResult.Error(Exception("Don't care"))
+            }
+            val result = LoadResult.Page(
+                data = data,
+                prevKey = args.prevKey,
+                nextKey = args.nextKey,
+                itemsBefore = args.itemsBefore,
+                itemsAfter = args.itemsAfter
+            )
+            val first = data.firstOrNull()?.id
+            val p = params::class.simpleName?.first()
+            Log.d("CHEESE_SRC", "load($p key=${params.key}, loadSize=${params.loadSize}) -> $args, data=$first (${data.count()})")
+            return result
         }
     }
 
